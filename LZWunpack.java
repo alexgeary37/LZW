75d2cbbc209df61ccc3e29dd0c66dd7fbee0c3bc
alexgeary37 <48072930+alexgeary37@users.noreply.github.com>
2019-06-22 17:19:25 +1200

    Update LZWunpack.java

--- LZWunpack.java
+++ LZWunpack.java
@@ -5,59 +5,60 @@
 	Program receives a stream of bytes as input. The stream of bytes 
 	is a list of integers representing phrase numbers for an LZW trie. 
 	The number of bits used to represent each phrase number is basically 
-	log2(number of phrases) in the trie. Each phrase number is essentially 
-	represented in binary format but -1. So 4 is represented as 11 instead 
-	of 100.
+	log2(number of phrases) in the trie. Each phrase number is represented 
+	in binary format.
 */
 
 import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileReader;
-import java.io.BufferedReader;
+import java.util.Scanner;
 
 
 public class LZWunpack{
 	private static BufferedInputStream inputStream; // reads stdin
-	private static BufferedReader fileReader; // reads from triesetup file
 	private static int numPhrases; // number of possible phrases in trie
-	private static int outputInt;
-	private static int outputIntBitsUsed;
-	private static int counter;
+	
+	// holds left over bits which partially made the phrase number
+	private static int leftOverOutputBits;
+	private static int numLeftOverOutputBits; // number of bits
 	
 	
 	public static void main(String[] args){
+		leftOverOutputBits = 0;
+		numLeftOverOutputBits = 0;
 		inputStream = new BufferedInputStream(System.in);
-		outputInt = 0;
-		outputIntBitsUsed = 0;
-		counter = 1;
 		
 		try{
 			
-			/* number of initial phrases in trie + the 
-			one gained from skipping first phrase number */
-			numPhrases = getNumPhrases();
+			// output all the unique bytes
+			outputUniqueBytes();
 			
 			outputPhraseNumber(1); // output first phrase number
 			
-			// read all of input stream
-			int input = -1; // will contain the next byte of input
-			int preparationInt = 0; // to hold 4 bytes of input
-			int offset = 24; // shift input left offset amount
+			int input = -1; // contains the next byte of input
+			int offset = 24; // offset amount for filling preparationInt with input
 			
-			int test = 0;
+			/* holds 4 bytes of input which, once either full or the input 
+			has ended, can be prepared for output, numPhrases bits at a time */
+			int preparationInt = 0;
+			
+			// read all of input stream
 			while((input = inputStream.read()) != -1){
+				
+				// fill prepInt from left to right with each input byte
 				preparationInt = preparationInt | input << offset;
-				offset -= 8;
-				if(counter == 259) return; // testing for BMPIMAGE unpacking
+				offset -= 8; // update offset to shift next input byte to the left by
+				
+				/* 4 bytes have been read in to preparationInt so 
+				it's ready to be output */
 				if(offset == -8){
-					prepareOutput(preparationInt, offset);
+					outputPreparationInt(preparationInt, 32);
 					preparationInt = 0;
 					offset = 24;
-					test++;
 				}
 			}
 			
-			prepareOutput(preparationInt, offset);
+			// finish output with whatever remains in the preparationInt
+			finishOutput(preparationInt, offset);
 			
 		}catch(Exception e){
 			e.printStackTrace(); // print exception information
@@ -65,74 +66,87 @@ public class LZWunpack{
 	}
 	
 	
+	// returns the number of phrases the trie is initialized with
+	private static void outputUniqueBytes() throws Exception{
+		String s = "";
+		int numPhraseInput = 0;
+		while((numPhraseInput = inputStream.read()) != 13)
+			s += Integer.toString(numPhraseInput-48);
+		
+		numPhrases = Integer.parseInt(s); // initialize number of phrases
+		
+		System.out.println(numPhrases); // print number of unique bytes
+		
+		inputStream.read(); // read the \n character from println in LZWpack
+		
+		// write all unique bytes to output stream
+		int i = 0;
+		while(i++ < numPhrases){
+			int input = inputStream.read();
+			System.out.println(input); // write unique bytes
+		}
+	}
+	
 	/* returns the number of bits required for the phrase number, 
 	log2(y) bits, where y is the number of phrases 
 	currently read in */
 	private static int getPhraseNumBitCount(int y){
 		int x = 0;
-		while((Math.pow(2, x)) < y) x++;
+		while((Math.pow(2, x)) <= y) x++;
 		return x;
 	}
 	
-	// finds the phrase number to output
-	private static void prepareOutput(int preparationInt, int offset){
-		int prepInt = preparationInt;
+	// finishes outputting the preparationInt
+	private static void finishOutput(int preparationInt, int offset){
 		int numPrepBits;
+		
+		// this covers any anomaly which may occur at end of unpacking
 		if(offset == -8) numPrepBits = 32;
 		else if(offset == 0) numPrepBits = 24;
 		else if(offset == 8) numPrepBits = 16;
 		else if(offset == 16) numPrepBits = 8;
 		else numPrepBits = 0;
 		
-		// testing for BMPIMAGE unpacking
-		if(counter >= 254)System.out.println(Integer.toBinaryString(prepInt)+" initial "+offset);
-
-		// while the number of bits in prepInt > numBits
-		while(numPrepBits >= getPhraseNumBitCount(numPhrases)-outputIntBitsUsed){
-			int numBits = getPhraseNumBitCount(numPhrases)-outputIntBitsUsed;
-			
-			int outputNumber = outputInt >>> 32-(outputIntBitsUsed+numBits);
-			outputNumber = outputNumber | prepInt >>> 32-numBits;
-			
-			if(outputNumber == 0) break;
-			
-			outputIntBitsUsed = outputInt = 0;
-			
-			outputPhraseNumber(outputNumber);
-			
-			prepInt = prepInt << numBits;
-			
-			// testing for BMPIMAGE unpacking
-			if(counter >= 254)System.out.println(Integer.toBinaryString(prepInt));
-			
-			numPrepBits -= numBits;
-		}
-		
-		if(numPrepBits != 0){
-			outputInt = prepInt;
-			outputIntBitsUsed = numPrepBits;
-		}else{
-			
-		}
+		outputPreparationInt(preparationInt, numPrepBits);
 	}
 	
-	// returns the number of phrases the trie is initialized with
-	private static int getNumPhrases() throws Exception{
-		File f = new File("trieSetup.txt");
-		fileReader = new BufferedReader(new FileReader(f));
+	/* outputs numPhrases worth of bits from the preparationInt 
+	which has just been filled by a number of bytes of input */
+	private static void outputPreparationInt(int preparationInt, int numPrepBitsLeft){
+		int prepInt = preparationInt;
+		int prepBitsRemaining = numPrepBitsLeft;
 		
-		int numPhrases = 0;
-		String line;
-		while((line = fileReader.readLine()) != null) numPhrases++;
+		/* while the number of bits in the preparation int 
+		is more than num bits to output */
+		while(prepBitsRemaining >= getPhraseNumBitCount(numPhrases)-numLeftOverOutputBits){
+			
+			// number of bits which consitute the output number
+			int numBits = getPhraseNumBitCount(numPhrases)-numLeftOverOutputBits;
+			int outputNumber = leftOverOutputBits >>> 32-(numLeftOverOutputBits+numBits);
+			outputNumber = outputNumber | prepInt >>> 32-numBits;
+			numLeftOverOutputBits = leftOverOutputBits = 0;
+			
+			/* there will never be a zero phrase number 
+			so this means unpacking is finished */
+			if(outputNumber == 0) break;
+			
+			outputPhraseNumber(outputNumber); // output number to stdout
+			
+			// update prepInt and the number of bits left to output
+			prepInt = prepInt << numBits;
+			prepBitsRemaining -= numBits;
+		}
 		
-		fileReader.close();
-		return numPhrases;
+		// save bits for following round of output
+		if(prepBitsRemaining != 0){
+			leftOverOutputBits = prepInt;
+			numLeftOverOutputBits = prepBitsRemaining;
+		}
 	}
 	
 	// prints the phrase number to standard out
 	private static void outputPhraseNumber(int phraseNum){
 		System.out.println(phraseNum);
-		numPhrases++;
-		counter++;
+		numPhrases++; // increment number of possible phrases in trie
 	}
 }
