36b47ba07623bb7f90eba7ee20d5461c7d747425
alexgeary37 <48072930+alexgeary37@users.noreply.github.com>
2019-10-31 18:29:45 +1300

    Update LZWencode.java
    combined encode and pack into encode

--- LZWencode.java
+++ LZWencode.java
@@ -3,6 +3,7 @@ Alex Geary
 1188083
 
 LZW encoder:
+
 Program initializes a trie structure with all the unique bytes read
 in from a file which contains all the unique bytes from the file to
 be compressed. Program receives standard input of a file and proceeds
@@ -11,6 +12,7 @@ a list of phrase numbers which will be compressed by a bit packer.
 */
 
 import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.util.ArrayList;
@@ -20,7 +22,7 @@ public class LZWencode {
     private static LZWTrie trie; // LZW trie structure
     private static BufferedInputStream inputStream; // stdin reader reads file to be compressed
     private static FileInputStream fileInputStream; // reads from file to be compressed
-
+    private static LZWpack packer;
 
     public static void main(String[] args) {
         if (args.length != 1) {
@@ -29,31 +31,46 @@ public class LZWencode {
 
             // create initial try structure and input stream
             trie = new LZWTrie();
+            packer = new LZWpack();
             inputStream = new BufferedInputStream(System.in);
 
             try {
 
-                if (!initializeTrie(args[0])) return;
-                
+                boolean initialize = initializeTrie(args[0]);
+                if (initialize == false) {
+                    return;
+                }
+
                 /* prints the number of unique bytes in the trie
                 followed by all unique bytes for LZWpack */
-                trie.printUniqueBytes();
+                packer.outputUniqueBytes(trie.getUniqueBytes());
 
                 int input = -1; // will contain the next byte of input
-                byte next = -1;
-                
-                /* while the end of input stream hasn't been reached,
-                perform LZ78 encoding on input stream */
+
+                // get past the first phrase num output which is predetermined to be '1'
                 while ((input = inputStream.read()) != -1) {
-                    next = (byte) input;
-                    trie.findByte(next);
+                    trie.findByte((byte) input);
+                    
                     if (trie.hasOutput()) {
-                        trie.dumpOutput();
-                        trie.findByte(next);
+                        trie.findByte((byte) input);
+                        break;
                     }
                 }
 
-                trie.finishOutput(next);
+                /* while the end of input stream hasn't been reached,
+                perform LZ78 encoding on input stream */
+                while ((input = inputStream.read()) != -1) {
+                    trie.findByte((byte) input);
+                    
+                    if (trie.hasOutput()) {
+                        packer.prepareOutput(trie.getOutput());
+                        trie.findByte((byte) input);
+                    }
+                }
+
+                // finish output
+                packer.prepareOutput(trie.finishOutput((byte) input));
+                packer.finishOutput();
 
                 inputStream.close(); // close inputStream
 
@@ -76,8 +93,11 @@ public class LZWencode {
         fileInputStream = new FileInputStream(f);
 
         int input = 0;
-        while ((input = fileInputStream.read()) != -1)
-            if (!initialPhrases.contains(input)) initialPhrases.add(input);
+        while ((input = fileInputStream.read()) != -1) {
+            if (!initialPhrases.contains(input)) {
+                initialPhrases.add(input);
+            }
+        }
 
         fileInputStream.close();
 
@@ -90,6 +110,7 @@ public class LZWencode {
 
 /*
 LZWTrie datastructure:
+
 Datastructure is a self referential datastructure which includes
 a reference to an array of itself i.e an array of trie's. The array of
 trie's references branches off of the current trie.
@@ -97,8 +118,8 @@ Whenever a new phrase is found in the trie, the number of that phrase
 is saved as an int called phraseNumOutput. This is overwritten when the
 next phrase is found in the trie. When there appears a mismatch, a new
 trie is added as a branch off of the current trie and its phrase number
-equals n, where n is the number of phrases in the trie. */
-
+equals n, where n is the number of phrases in the trie.
+*/
 class LZWTrie {
     // variables for each instance of Trie
     private int phraseNumber; // the number for each phrase in trie
@@ -150,7 +171,9 @@ class LZWTrie {
                 
                 i++;
                 
-                if (i < currentTrie.numBranches) current = currentTrie.branches[i];
+                if (i < currentTrie.numBranches) {
+                    current = currentTrie.branches[i];
+                }
 
             } while (i < currentTrie.numBranches);
         }
@@ -162,8 +185,9 @@ class LZWTrie {
 
     // adds a new phrase to the trie
     public void addPhrase(byte mismatch) {
-        if (currentTrie.numBranches == currentTrie.branches.length)
+        if (currentTrie.numBranches == currentTrie.branches.length) {
             increaseBranches(); // handle number of branches is exceeding set length
+        }
 
         // add new trie to the branches
         numPhrases++;
@@ -173,8 +197,9 @@ class LZWTrie {
 
     // searches trie for byte and adds it if it hasn't already been added
     public void initialize(ArrayList<Integer> phrases) {
-        if (currentTrie.numBranches == currentTrie.branches.length)
+        if (currentTrie.numBranches == currentTrie.branches.length) {
             increaseBranches(); // handle number of branches is exceeding set length
+        }
 
         // add new trie to the branches
         for (Integer phrase : phrases) {
@@ -203,20 +228,30 @@ class LZWTrie {
 
     /* prints the number of unique bytes in the trie
     followed by all unique bytes for setting up LZWpack */
-    public void printUniqueBytes() {
-        System.out.println(numBranches);
-        for (int i = 0; i < numBranches; i++) System.out.println(branches[i].mismatch);
+    public ArrayList<Integer> getUniqueBytes() throws Exception {
+        ArrayList<Integer> uniqueBytes = new ArrayList<>();
+
+        uniqueBytes.add(numBranches);
+        for (int i = 0; i < numBranches; i++) {
+            int mismatch = (int) branches[i].mismatch;
+            uniqueBytes.add(mismatch);
+        }
+
+        return uniqueBytes;
     }
 
-    // prints the output phrase number
-    public void dumpOutput() throws Exception {
-        System.out.println(phraseNumOutput); // print the phrase number
+    // returns the output phrase number
+    public int getOutput() throws Exception {
+        return phraseNumOutput;
     }
 
-    // prints the final phrase number
-    public void finishOutput(byte b) throws Exception {
-        if (currentTrie == this) findLastByte(b);
-        dumpOutput();
+    // returns the final phrase number
+    public int finishOutput(byte b) throws Exception {
+        if (currentTrie == this) {
+            findLastByte(b);
+        }
+
+        return phraseNumOutput;
     }
 
     // finds the last byte and sets the output phrase number
@@ -236,8 +271,123 @@ class LZWTrie {
     // increases the capacity of array for currentTrie branches
     private void increaseBranches() {
         LZWTrie[] temp = new LZWTrie[currentTrie.numBranches * 2];
-        for (int i = 0; i < currentTrie.numBranches; i++)
+        for (int i = 0; i < currentTrie.numBranches; i++) {
             temp[i] = currentTrie.branches[i];
+        }
         currentTrie.branches = temp;
+    }
+}
+
+/*
+LZW bit packer:
+
+Program receives input as a stream of numbers, one per line created by the LZW encoder.
+Each number is a phrase number which is output in log2p bits where p is the number of
+phrases that the encoder has at that time of its output.
+*/
+class LZWpack {
+    private static BufferedOutputStream outputStream; // writes to stdout
+    private static int numPhrases; // to keep track of how many bits the phrase number needs to be packed with
+    private static int numFreeOutputBits; // keeps track of how many bits are left to fill in output int
+    private static int outputInt; // int to be filled with packed bits for output
+
+
+    public LZWpack() {
+        numFreeOutputBits = 32;
+        outputInt = 0;
+        outputStream = new BufferedOutputStream(System.out);
+    }
+
+    // returns the number of phrases the trie is initialized with
+    public boolean outputUniqueBytes(ArrayList<Integer> uniqueBytes) throws Exception {
+        numPhrases = uniqueBytes.get(0);
+
+        System.out.println(numPhrases); // print number of unique bytes
+
+        // write all unique bytes to output stream
+        int i = 1;
+        while (i <= numPhrases) {
+            int b = uniqueBytes.get(i);
+            outputStream.write((byte) b); // write unique byte
+            outputStream.flush();
+            i++;
+        }
+
+        numPhrases++;
+        return true;
+    }
+
+    /* returns the number of bits required for the phrase number,
+    log2(y) bits, where y is the number of phrases currently read in */
+    private int getPhraseNumBitCount(int y) {
+        int x = 0;
+        while ((Math.pow(2, x)) <= y) {
+            x++;
+        }
+        return x;
+    }
+
+    // prepares output int to be filled with phrase number and mismatch bits
+    public void prepareOutput(int phraseNum) throws Exception {
+        int numPhraseBits = getPhraseNumBitCount(numPhrases); // num bits for phrase number
+
+        // pack the output int with the phrase number
+        packPhrase(numPhraseBits, phraseNum);
+
+        numPhrases++; // increment num phrases in the LZW trie
+    }
+
+    // packs output int with compressed phrase number bits
+    private void packPhrase(int numPhraseBits, int phraseNum) throws Exception {
+        if (numFreeOutputBits >= numPhraseBits) {
+
+            // copy phraseNum bits into output int and update number of unused bits
+            outputInt = outputInt | phraseNum << (numFreeOutputBits - numPhraseBits);
+            numFreeOutputBits = numFreeOutputBits - numPhraseBits;
+        } else {
+
+            int shiftRightAmount = numPhraseBits - numFreeOutputBits;
+
+            // fill rest of output int with limited num of phraseNum bits
+            outputInt = outputInt | phraseNum >>> shiftRightAmount;
+            writeOutput(4);
+            
+            /* copy remaining phraseNum bits into output int
+            and update the number of unused bits */
+            outputInt = phraseNum << 32 - shiftRightAmount;
+            numFreeOutputBits -= shiftRightAmount;
+        }
+    }
+
+    // output whatever is remaining in the outputInt
+    public void finishOutput() throws Exception {
+        if (numFreeOutputBits < 32) {
+            if (((32 - numFreeOutputBits) % 8) == 0) {
+                writeOutput((32 - numFreeOutputBits) / 8);
+            } else {
+                writeOutput((32 - numFreeOutputBits) / 8 + 1);
+            }
+        }
+        
+        outputStream.close();
+    }
+
+    // writes 4 bytes to output stream, possibly less if end of input has been reached
+    private void writeOutput(int numBytes) throws Exception {
+        int shiftAmount = 24;
+        byte[] output = new byte[numBytes];
+
+        // copy bits from output int into 4 separate bytes for writing to standard out
+        for (int i = 0; i < output.length; i++) {
+            output[i] = (byte) (outputInt >>> shiftAmount);
+            shiftAmount -= 8;
+        }
+
+        outputStream.write(output);
+        outputStream.flush();
+
+        // clear all outputint bits and update number of unused bits
+        outputInt = 0;
+        numFreeOutputBits = 32;
     }
 }
